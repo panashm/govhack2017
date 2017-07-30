5c0124ad13b26dc02c91eed712faae0bc367e408
Panashe Mutema <Panashe@Panashes-MacBook-Pro.local>
2017-07-30 15:02:37 +1000

    added 2014,15,16 data to chart

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from __future__ import print_function # In python 2.7
+from __future__ import print_function, division # In python 2.7
 from flask import render_template, flash, redirect, session, url_for, request, g
 from urlparse import urlparse, urljoin
 import smtplib
@@ -10,6 +10,41 @@ from datetime import datetime
 from app import app, db, lm, admin, BaseView, expose, ModelView, bcrypt
 #from .forms import searchForm, newEntryForm, item_choices, day_choices, search_choices, loginForm
 from .models import User
+import pandas as pd
+import numpy as np
+
+subjects=[15210, 15240, 15360, 15120, 15050]
+
+filenames = ["data/2014.csv", "data/2015.csv", "data/2016.csv"] # Fill in remaining files.
+df1 = pd.DataFrame()
+for filename in filenames:
+    df1 = df1.append(pd.read_csv(filename))
+    
+subjectDict = {}
+
+num = 0
+for n in range(5):
+    print("hello world")
+    subjectDict[subjects[num]] = []
+    print(subjects[num])
+    year = 2014
+    for n in range(3):
+        df1_filtered = df1[(df1['subject'] == subjects[num]) & (df1['year'] == year)]
+        #print(df1_filtered)
+        totalF = df1_filtered['t_female'].sum()
+        print("total female",year, totalF)
+
+        totalM = df1_filtered['t_male'].sum()
+        print("total male",year, totalM)
+
+        if totalF or totalM > 0:
+            proportion = (totalF/(totalF+totalM))*100
+            print("proportion is", proportion, totalF, totalM)
+        year += 1
+        subjectDict[subjects[num]].append(proportion)
+    num +=1
+    
+print(subjectDict)
 
 class MyView(BaseView):
     @expose('/')
@@ -25,10 +60,13 @@ def is_safe_url(target):
            ref_url.netloc == test_url.netloc
 
 
+#function to get subject
 #route for the main page    
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    
+    
+    return render_template('index.html', subjectDict = subjectDict)
 
 
 @app.route("/about", methods=['GET', 'POST'])
